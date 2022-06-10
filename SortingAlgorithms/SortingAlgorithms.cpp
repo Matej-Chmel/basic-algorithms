1a216970d7e9dbb3bc98e56c783b4fb8271599dc
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-06-10 22:52:18 +0200

    Code reformatted.

--- SortingAlgorithms/SortingAlgorithms.cpp
+++ SortingAlgorithms/SortingAlgorithms.cpp
@@ -6,33 +6,38 @@
 #include <string>
 #include <vector>
 
-class AppError : public std::exception {
+class AppError final : public std::exception {
 public:
 	explicit AppError(const std::string& s);
-	template <typename T>
-	AppError operator<<(const T& t);
+	template<typename T> auto operator<<(const T& t) -> AppError;
 };
 
 enum class SortAlgoType {
 	BUBBLE,
 	INSERTION,
+	MERGE,
 	SELECTION
 };
 
-std::string to_string(const SortAlgoType t);
+auto to_string(SortAlgoType t) -> std::string;
 
-static std::ostream& operator<<(std::ostream& s, const std::vector<int>& v);
-static void bubble_sort(std::vector<int>& v);
-static std::vector<int> generate_int_vector(const size_t size, const int min, const int max);
-static void insertion_sort(std::vector<int>& v);
-static bool is_sorted(const std::vector<int>& v);
-static void run_all_algos(const size_t size);
-static void run_test(const size_t size, const SortAlgoType type);
-static void run_with_sizes(const std::vector<int>& sizes);
-static void selection_sort(std::vector<int>& v);
+static auto operator<<(
+	std::ostream& s,
+	const std::vector<int>& v
+) -> std::ostream&;
+static auto bubble_sort(std::vector<int>& v) -> void;
+static auto generate_int_vector(
+	size_t size, int min, int max
+) -> std::vector<int>;
+static auto insertion_sort(std::vector<int>& v) -> void;
+static auto is_sorted(const std::vector<int>& v) -> bool;
+static auto run_all_algos(size_t size) -> void;
+static auto run_test(size_t size, SortAlgoType type) -> void;
+static auto run_with_sizes(const std::vector<int>& sizes) -> void;
+static auto selection_sort(std::vector<int>& v) -> void;
 
-std::ostream& operator<<(std::ostream& s, const std::vector<int>& v) {
-	if (v.empty()) {
+auto operator<<(std::ostream& s, const std::vector<int>& v) -> std::ostream& {
+	if(v.empty()) {
 		s << "EMPTY";
 		return s;
 	}
@@ -40,15 +45,14 @@ std::ostream& operator<<(std::ostream& s, const std::vector<int>& v) {
 	const auto last_idx = v.size() - 1;
 	s << '[';
 
-	for (size_t i = 0; i < last_idx; i++)
+	for(size_t i = 0; i < last_idx; i++)
 		s << v[i] << ", ";
 
 	s << v[last_idx] << ']';
 	return s;
 }
 
-template <typename T>
-AppError AppError::operator<<(const T& t) {
+template<typename T> auto AppError::operator<<(const T& t) -> AppError {
 	std::stringstream stream;
 	stream << this->what() << t;
 	return AppError(stream.str());
@@ -63,17 +67,17 @@ AppError::AppError(const std::string& s) : exception(s.c_str()) {}
  *
  * Space complexity: O(1)
  */
-void bubble_sort(std::vector<int>& v) {
+auto bubble_sort(std::vector<int>& v) -> void {
 	const auto size = v.size();
 	const auto last_idx = size - 1;
 
-	for (size_t i = 1; i < size; i++) {
+	for(size_t i = 1; i < size; i++) {
 		auto swapped = false;
 
-		for (size_t j = 0; j < last_idx; j++) {
+		for(size_t j = 0; j < last_idx; j++) {
 			const auto next_idx = j + 1;
 
-			if (v[j] > v[next_idx]) {
+			if(v[j] > v[next_idx]) {
 				const auto temp = v[j];
 				v[j] = v[next_idx];
 				v[next_idx] = temp;
@@ -81,18 +85,22 @@ void bubble_sort(std::vector<int>& v) {
 			}
 		}
 
-		if (!swapped)
+		if(!swapped)
 			break;
 	}
 }
 
-std::vector<int> generate_int_vector(const size_t size, const int min, const int max) {
+auto generate_int_vector(
+	const size_t size,
+	const int min,
+	const int max
+) -> std::vector<int> {
 	std::default_random_engine gen(std::random_device{}());
 	const std::uniform_int_distribution<int> dist(min, max);
 	std::vector<int> res;
 	res.reserve(size);
 
-	for (size_t i = 0; i < size; i++)
+	for(size_t i = 0; i < size; i++)
 		res.emplace_back(dist(gen));
 
 	return res;
@@ -102,64 +110,71 @@ std::vector<int> generate_int_vector(const size_t size, const int min, const int
  * Time complexity: O(n^2)
  * Space complexity: O(1)
  */
-void insertion_sort(std::vector<int>& v) {
+auto insertion_sort(std::vector<int>& v) -> void {
 	const auto size = static_cast<int>(v.size());
 
-	if (size < 2)
+	if(size < 2)
 		return;
 
-	for (int i = 1; i < size; i++) {
-		int j = i - 1;
+	for(auto i = 1; i < size; i++) {
+		auto j = i - 1;
 		const auto val = v[i];
-		
-		for(;j > 0 && v[j] > val; j--)
+
+		for(; j > 0 && v[j] > val; j--)
 			v[j + 1] = v[j];
 
 		v[j] = val;
 	}
 }
 
-bool is_sorted(const std::vector<int>& v) {
-	if (v.size() < 2)
+auto is_sorted(const std::vector<int>& v) -> bool {
+	if(v.size() < 2)
 		return true;
 
 	const auto last_idx = v.size() - 1;
 
-	for (size_t i = 1; i < last_idx; i++)
-		if (v[i] < v[i - 1])
+	for(size_t i = 1; i < last_idx; i++)
+		if(v[i] < v[i - 1])
 			return false;
 	return true;
 }
 
-void run_all_algos(const size_t size) {
+auto merge_sort(std::vector<int>& v) -> void {}
+
+auto run_all_algos(const size_t size) -> void {
 	run_test(size, SortAlgoType::BUBBLE);
 	run_test(size, SortAlgoType::INSERTION);
 	run_test(size, SortAlgoType::SELECTION);
 	std::cout << "[PASSED] " << "Test size: " << size << '\n';
 }
 
-void run_test(const size_t size, const SortAlgoType type) {
+auto run_test(const size_t size, const SortAlgoType type) -> void {
 	auto vec = generate_int_vector(size, 0, 100);
 	const auto vec_copy = vec;
 
-	switch (type) {
+	switch(type) {
 	case SortAlgoType::BUBBLE:
 		bubble_sort(vec);
 		break;
 	case SortAlgoType::INSERTION:
 		insertion_sort(vec);
 		break;
+	case SortAlgoType::MERGE:
+		merge_sort(vec);
+		break;
 	case SortAlgoType::SELECTION:
 		selection_sort(vec);
 		break;
 	}
 
-	if (!is_sorted(vec))
-		throw AppError("[FAILED] {vec: ") << vec_copy << ", " << "algo: " << to_string(type) << '}';
+	if(!is_sorted(vec)) {
+		throw AppError("[FAILED] {vec: ") << vec_copy << ", " << "algo: " <<
+			to_string(type) << '}';
+	}
 }
 
-void run_with_sizes(const std::vector<int>& sizes) {
-	for (const auto& s : sizes)
+auto run_with_sizes(const std::vector<int>& sizes) -> void {
+	for(const auto& s : sizes)
 		run_all_algos(s);
 }
 
@@ -167,20 +182,20 @@ void run_with_sizes(const std::vector<int>& sizes) {
  * Time complexity, all cases: O(n^2).
  * Space complexity: O(1).
  */
-void selection_sort(std::vector<int>& v) {
+auto selection_sort(std::vector<int>& v) -> void {
 	const auto size = v.size();
 
-	if (size < 2)
+	if(size < 2)
 		return;
 
-	for (size_t i = 0; i < size; i++) {
+	for(size_t i = 0; i < size; i++) {
 		auto min_idx = i;
 
-		for (size_t j = i; j < size; j++)
-			if (v[j] < v[min_idx])
+		for(auto j = i; j < size; j++)
+			if(v[j] < v[min_idx])
 				min_idx = j;
 
-		if (min_idx != i) {
+		if(min_idx != i) {
 			const auto temp = v[i];
 			v[i] = v[min_idx];
 			v[min_idx] = temp;
@@ -188,21 +203,23 @@ void selection_sort(std::vector<int>& v) {
 	}
 }
 
-std::string to_string(const SortAlgoType t) {
-	switch (t) {
+auto to_string(const SortAlgoType t) -> std::string {
+	switch(t) {
 	case SortAlgoType::BUBBLE:
 		return "BUBBLE";
 	case SortAlgoType::INSERTION:
 		return "INSERTION";
+	case SortAlgoType::MERGE:
+		return "MERGE";
 	case SortAlgoType::SELECTION:
 		return "SELECTION";
 	}
 	throw AppError("[UNKNOWN VALUE] SortAlgoType = ") << static_cast<int>(t);
 }
 
-int main() {
+auto main() -> int {
 	try { run_with_sizes({0, 1, 2, 10, 15, 25}); }
-	catch (const std::exception& e) {
+	catch(const std::exception& e) {
 		std::cerr << e.what() << '\n';
 		return EXIT_FAILURE;
 	}
